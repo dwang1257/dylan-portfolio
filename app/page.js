c396388ba5a26d14884dafd5bbe46e7953a38979
Dylan Wang <97680620+dwang1257@users.noreply.github.com>
2025-07-08 21:08:36 -0400

    different font

--- app/page.js
+++ app/page.js
@@ -15,11 +15,11 @@ export default function Home() {
       {/* Main Content */}
       <main className="font-inter flex-1 flex items-center px-8 sm:px-12 pb-32">
         <div className="max-w-7xl space-y-10 sm:space-y-12">
-          <h1 className="text-5xl sm:text-6xl font-light leading-relaxed animate__animated animate__fadeInUp">
+          <h1 className="text-5xl sm:text-6xl font-semibold leading-relaxed animate__animated animate__fadeInUp">
             Hi, I&apos;m Dylan Wang.
           </h1>
 
-          <div className="max-w-7xl space-y-8 text-xl sm:text-2xl font-light leading-relaxed text-gray-200">
+          <div className="max-w-7xl space-y-8 text-xl sm:text-2xl font-semibold leading-relaxed text-gray-200">
             <p className="animate__animated animate__fadeInUp animate__delay-1s">
               Currently working on the Common Platform Team at{' '}
                 <a 
